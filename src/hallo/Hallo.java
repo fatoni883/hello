7718f9a4941e304a5f18989f1f8c1766d71af2f9
fatoni883 <mimamfatoni2@gmail.com>
2025-03-14 11:53:01 -0400

    add hello world

--- src/hallo/Hallo.java
+++ src/hallo/Hallo.java
@@ -15,6 +15,8 @@ public class Hallo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world")
+                ;
     }
     
 }
